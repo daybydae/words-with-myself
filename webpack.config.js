f256f0f4a76b0016defc084e56b1de9420ca06cd
Dae Kim <dae.dkim6@gmail.com>
2018-01-17 14:51:34 -0500

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+    entry: [
+        './lib/main.js'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'lib/')
+    },
+    devtool: 'source-map',
+    resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
